74cd71f8cf96f2dff0fcd953b0041b0d2f298f02
hanxlinsist <xinglongsuccess@outlook.com>
2016-11-27 20:51:42 +0800

    add Reverse Linked List/

---
+++ Reverse Linked List/README.md
@@ -0,0 +1,17 @@
+# 题目链接
+
+[Reverse Linked List](https://leetcode.com/problems/reverse-linked-list/)
+
+# 解题思路
+
+leetcode官方已经给出很详细的解题思路，这里我就不再重复了。[解题思路](https://leetcode.com/articles/reverse-linked-list/)
+
+# 递归版本时间和空间复杂度
+
+- 时间复杂度：O(n)
+- 空间复杂度：O(n)
+
+# 迭代版本时间和空间复杂度
+
+- 时间复杂度：O(n)
+- 空间复杂度：O(1)
---
+++ Reverse Linked List/Solution.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+ 
+// 递归版本
+public ListNode reverseList(ListNode head) {
+    if (head == null || head.next == null) return head;
+    ListNode p = reverseList(head.next);
+    head.next.next = head;
+    head.next = null;
+    return p;
+}
+
+
+// 迭代版本
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = nextTemp;
+        }
+        return pre;
+    }
+}
+
+
